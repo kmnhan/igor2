d792810b79d045ea7cd0e01fe9650b9d121606a6
TeetotalingTom <114575548+TeetotalingTom@users.noreply.github.com>
2024-06-16 23:21:11 +0200

    Expand compatibility to older Python/NumPy versions (#16)
    * Updated compatibilities, helper, and tests
    
    * Updated compatibilities, helper, and tests

--- tests/helper.py
+++ tests/helper.py
@@ -1,9 +1,26 @@
 import pathlib
-from pprint import pformat
-
+import numpy as _numpy
+from pprint import pformat, PrettyPrinter
 from igor2.binarywave import load as loadibw
 
 data_dir = pathlib.Path(__file__).parent / "data"
+
+class NumpyPrettyPrinter(PrettyPrinter):
+    def format(self, object, context, maxlevels, level):
+        if isinstance(object, _numpy.ndarray):
+            # Generate the entire array string using numpy's own mechanism but ensuring dtype is displayed
+            array_repr = _numpy.array2string(object, separator=', ')
+            # Append dtype only once for the whole array
+            formatted_array = f"array({array_repr}, dtype='{object.dtype}')"
+            return (formatted_array, True, False)
+        # Default handling for other types
+        return super().format(object, context, maxlevels, level)
+
+
+def custom_pformat(object, indent=1, width=80, depth=None, *, compact=False):
+    printer = NumpyPrettyPrinter(indent=indent, width=width, depth=depth, compact=compact)
+    return printer.pformat(object)
+
 
 
 def assert_equal_dump_no_whitespace_no_byte(data_a, data_b):
@@ -12,11 +29,12 @@ def assert_equal_dump_no_whitespace_no_byte(data_a, data_b):
             [" ", ""],  # ignore whitespaces
             ["b'", "'"],  # ignore bytes vs str
             ["\n", ""],  # ignore newlines
-            # teat all floats as equal
+            # treat all floats as equal
             ["float32", "float"],
             ["float64", "float"],
             ["'>f4'", "float"],
             ["'>f8'", "float"],
+            ["'float'", "float"],
         ]:
             x = x.replace(old, new)
         return x
@@ -35,7 +53,7 @@ def dumpibw(filename):
 
 
 def format_data(data):
-    lines = pformat(data).splitlines()
+    lines = custom_pformat(data).splitlines()
     return '\n'.join([line.rstrip() for line in lines])
 
 
