112f6a842fee03c8f12b4292058f7631a174dc72
Paul Müller <dev@craban.de>
2024-06-16 23:45:36 +0200

    typo

--- igor2/__init__.py
+++ igor2/__init__.py
@@ -2,7 +2,7 @@
 """Interface for reading binary IGOR files."""
 try:
     from ._version import __version__, __version_tuple__
-except ImportError
+except ImportError:
     # semver not working on Python 3.6
     __version__ = "unknown"
 
